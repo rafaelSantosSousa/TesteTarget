f4c43cdeb3322b5ce7f214fdb3cfed9dac4103fb
rafaelSantosSousa <rafael.ti1@outlook.com>
2023-03-12 01:15:27 -0300

    Inserindo exercício 3

--- src/main/java/org/example/Exercicio_3.java
+++ src/main/java/org/example/Exercicio_3.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio_3 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de dias em que houve faturamento: ");
+        int n = sc.nextInt();
+        int d = 0;
+        double soma = 0;
+        double[] faturadias = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite o faturamento para o dia " + (d = (d + 1)) + ":");
+            faturadias[i] = sc.nextDouble();
+        }
+        for (int i = 0; i < n; i++) {
+            soma = soma + faturadias[i];
+        }
+        double media = (soma / n);
+
+        double maiorValor = Double.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (faturadias[i] > maiorValor) {
+                maiorValor = faturadias[i];
+            }
+        }
+
+        double menorValor = Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (faturadias[i] < menorValor) {
+                menorValor = faturadias[i];
+            }
+        }
+
+
+        System.out.println("O valor total do faturamento no mês: " + soma);
+        System.out.println("O maior valor de faturamento ocorrido em um dia do mês: " + maiorValor);
+        System.out.println("O menor valor de faturamento ocorrido em um dia do mês: " + menorValor);
+        System.out.printf("O valor da média mensal é: %.2f", media);
+
 
     }
-}
+    }
--- src/main/java/org/example/Exercicio_4.java
+++ src/main/java/org/example/Exercicio_4.java
@@ -1,12 +1,7 @@
 package org.example;
 
-import java.util.Scanner;
-
-import static java.text.NumberFormat.getPercentInstance;
-
 public class Exercicio_4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         double sp = 67.83643F;
         double rj = 36.67866F;
@@ -36,3 +31,13 @@ public class Exercicio_4 {
 
     }
 }
+//4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+//
+//SP – R$67.836,43
+//RJ – R$36.678,66
+//MG – R$29.229,88
+//ES – R$27.165,48
+//Outros – R$19.849,53
+//
+//Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
+// 
